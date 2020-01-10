0637d8b0a44e9cfca3dc7540329cb637cceca8c9
Juan Carlos Cardoso <papo@nati.games>
2020-01-09 22:16:57 -0600

    88. movement &  Time.deltaTime 89. ViewPortToWorldPoint()

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Game Settings (speed for player)
+    [SerializeField] float moveSpeed = 10f;
+    // Padding from Borders
+    [SerializeField] float padding = 1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupMoveBoundaries();
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        // What camera
+        Camera gameCamera = Camera.main;
+        // Get The value of the X element for our Viewport from Camera
+        // Refer to slides to understand Viewport pos (0,0:1,0:1,1:0,1)
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // refer to Edit>ProjSettings>Input (Using Keyboard arrows or alt)
+        // deltaTime used to make speed/distance frame independent.
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        // define NewPosition (and clap limits)
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);        
+        // move sideways
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
